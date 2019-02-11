0890613b7802bd1c36da2c06393a397ac0e4535d
e <1120317328@qq.com>
2019-02-11 20:43:18 +0800

    缺少node-odul

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var sql = require('./../tool/sql');
+var md5 = require('md5'); 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+	//	session方式
+	if(req.session.isLogin != 1) {
+		res.redirect('/login');
+		return;
+	}
+	
+  res.render('index', { activeIndex: 1 });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/loginAction', (req, res, next) => {
+	let { username, password } = req.body;
+	password = md5(password);
+	sql.find('sh1811', 'admin', { username, password }).then(data => {
+		if (data.length === 0) {
+//			res.cookie('isLogin', 0);		// cookie方式
+			res.session.isLogin = 0;
+			res.redirect('/login');
+		} else {
+//			res.cookie('isLogin', 1);   // cookie方式
+			req.session.isLogin = 1;
+			res.redirect('/');
+		}
+	})
+})
+
+router.get('/logout', (req, res, next) => {
+//	req.session.isLogin = 0;
+	req.session.isLogin = 1;
+	res.redirect('/login');
+})
+
+module.exports = router;
